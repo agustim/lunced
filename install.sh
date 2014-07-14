fba45fe1562dde88ff2b2fa1f4e19c919e675467
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-07-14 09:32:26 +0200

    Enable lunced daemonization (save PID) to file

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 INSTALLDIR="/usr/share/lunced"
 LUALIB="//usr/lib/lua/lunced"
 CGIDIR="/www/cgi-bin"
+INITDIR"/etc/init.d"
 
 mkdir -p $INSTALLDIR
 cp *.lua $INSTALLDIR/
@@ -12,5 +13,8 @@ cp lunced/*.lua $LUALIB/
 
 mkdir -p $CGIDIR
 cp cgi-bin/* $CGIDIR/
+
+mkdir -p $INITDIR
+cp init.d/* $INITDIR/
 
 
